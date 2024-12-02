eed2b55879f8f45a793821186fd5329ba42b8c93
Ki98 <mgs5014@naver.com>
2024-12-02 12:39:13 +0900

    9
    999

--- js/header.js
+++ js/header.js
@@ -7,14 +7,14 @@ document.addEventListener("DOMContentLoaded", function () {
       event.preventDefault(); // 링크 이동 방지
 
       // 모든 submenu를 숨김
-      const allSubmenus = document.querySelectorAll(".submenu");
+      const allSubmenus = document.querySelectorAll(".submenu1");
       allSubmenus.forEach((submenu) => {
         submenu.style.opacity = "0";
         submenu.style.visibility = "hidden";
       });
 
       // 현재 클릭된 항목의 submenu 표시/숨김 토글
-      const submenu = this.querySelector(".submenu");
+      const submenu = this.querySelector(".submenu1");
       if (submenu.style.visibility === "visible") {
         submenu.style.opacity = "0";
         submenu.style.visibility = "hidden";
@@ -28,11 +28,25 @@ document.addEventListener("DOMContentLoaded", function () {
   // 페이지의 다른 곳을 클릭하면 메뉴 닫기
   document.addEventListener("click", function (event) {
     if (!event.target.closest(".hasChild")) {
-      const allSubmenus = document.querySelectorAll(".submenu");
+      const allSubmenus = document.querySelectorAll(".submenu1");
       allSubmenus.forEach((submenu) => {
         submenu.style.opacity = "0";
         submenu.style.visibility = "hidden";
       });
     }
+  });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const categoryLinks = document.querySelectorAll(".category-link");
+
+  categoryLinks.forEach((link) => {
+    link.addEventListener("click", (event) => {
+      // 모든 링크에서 active 클래스 제거
+      categoryLinks.forEach((link) => link.classList.remove("active"));
+
+      // 현재 클릭된 링크에 active 클래스 추가
+      event.target.classList.add("active");
+    });
   });
 });
