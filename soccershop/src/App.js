32cca54c8413a00ba619d60af87b9e3a74375d02
Ki98 <mgs5014@naver.com>
2024-12-03 18:12:57 +0900

    10
    z

--- soccershop/src/App.js
+++ soccershop/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./components//Header/Header";
+import Main from "./components/Main/Main"
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Main />
     </div>
   );
 }
