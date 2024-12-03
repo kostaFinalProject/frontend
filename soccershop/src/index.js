8996f178920b0224df0eb3132aebe4e83fedde2d
Ki98 <mgs5014@naver.com>
2024-12-03 15:06:00 +0900

    9
    9999999

--- soccershop/src/index.js
+++ soccershop/src/index.js
@@ -1,4 +1,12 @@
-import ReactDOM from "react-dom";
-import { App } from "./App";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-ReactDOM.render(<App />,document.getElementById("root"))
+const rootElement = document.getElementById("root"); // index.html 파일에서 "root" div 요소를 찾음
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(
+  <React.StrictMode>
+    <App />  {/* App 컴포넌트 렌더링 */}
+  </React.StrictMode>
+);
